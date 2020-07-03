2a6b5bfe47e30d5d13e09e9dacdb8401544d196f
zyra zia <zyra.zia@gmail.com>
2020-07-03 11:34:19 +0100

    added anonymity set, fixed connect bug

--- src/components/ConnectButton.js
+++ src/components/ConnectButton.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-class Message extends React.Component {
+class ConnectButton extends React.Component {
 
     constructor(props) {
         super(props);
@@ -37,4 +37,4 @@ class Message extends React.Component {
     }
 }
 
-export default Message;
+export default ConnectButton;
